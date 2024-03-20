726d6cfdef832e3293969b951e88f2bc793ae543
sunilvelusuri07 <velusurisunilkumar@gmail.com>
2024-03-20 23:17:25 +0530

    ApiGateway service is added

--- student-service/src/main/java/com/techEnthusiast/studentservice/controller/StudentController.java
+++ student-service/src/main/java/com/techEnthusiast/studentservice/controller/StudentController.java
@@ -3,6 +3,9 @@ package com.techEnthusiast.studentservice.controller;
 import com.techEnthusiast.studentservice.dto.StudentDto;
 import com.techEnthusiast.studentservice.entity.Student;
 import com.techEnthusiast.studentservice.service.StudentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,8 +29,16 @@ public class StudentController {
     }
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name="studentservice", fallbackMethod="fallbackMethod")
+    @Retry(name="studentservice")
+    @TimeLimiter(name="studentservice")
     public CompletableFuture<String> getStudentWithDepartment(@PathVariable Long id) {
         return CompletableFuture.supplyAsync(()->studentService.getStudentWithDepartment(id));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CompletableFuture<String> fallbackMethod(@PathVariable Long id, RuntimeException ex) {
+        return CompletableFuture.supplyAsync(()->"Service is down. Please try after some time.");
     }
 
 }
